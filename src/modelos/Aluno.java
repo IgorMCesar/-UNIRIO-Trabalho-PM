62e47ba28cf777bb5402f4e6e4888d95a38298cf
Lucas L. Morais <lucas.lopes@uniriotec.br>
2018-11-25 22:16:03 -0200

    Aluno comparável

--- src/modelos/Aluno.java
+++ src/modelos/Aluno.java
@@ -1,9 +1,10 @@
 package modelos;
 
-public class Aluno {
+public class Aluno implements Comparable<Aluno> {
 
 	private String nome;
 	private Curriculo curriculo;
+	private int pontuacao;
 
 	public String getNome() {
 		return nome;
@@ -19,6 +20,29 @@ public class Aluno {
 
 	public void setCurriculo(Curriculo curriculo) {
 		this.curriculo = curriculo;
+	}
+
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+	@Override
+	public int compareTo(Aluno o) {
+		if (o.getPontuacao() < this.getPontuacao()) {
+			return -1;
+		} else if (o.getPontuacao() > this.getPontuacao()) {
+			return 1;
+		} 
+		//Retorna 2 caso o nome do aluno seja igual.
+		else if (this.getNome().equals(o.getNome())) {
+			return 2;
+		} else {
+		return 0;
+		}
 	}
 
 }
