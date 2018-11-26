3ac330d3a44a4df9561386642937ce5a807f21f9
Lucas L. Morais <lucas.lopes@uniriotec.br>
2018-11-25 22:35:45 -0200

    Implementação dos critérios de desempate

--- src/modelos/Aluno.java
+++ src/modelos/Aluno.java
@@ -33,15 +33,38 @@ public class Aluno implements Comparable<Aluno> {
 	@Override
 	public int compareTo(Aluno o) {
 		if (o.getPontuacao() < this.getPontuacao()) {
-			return -1;
-		} else if (o.getPontuacao() > this.getPontuacao()) {
 			return 1;
-		} 
-		//Retorna 2 caso o nome do aluno seja igual.
-		else if (this.getNome().equals(o.getNome())) {
-			return 2;
+		} else if (o.getPontuacao() > this.getPontuacao()) {
+			return -1;
 		} else {
-		return 0;
+			//CRITERIOS DE DESEMPATE
+			
+			//semestres cursados
+			if(o.getCurriculo().getSemestresCursados() < this.getCurriculo().getSemestresCursados()) {
+				return 1;
+			} else if(o.getCurriculo().getSemestresCursados() > this.getCurriculo().getSemestresCursados()) {
+				return -1;
+			} 
+			//artigos publicados
+			else if(o.getCurriculo().getArtigos().size() < this.getCurriculo().getArtigos().size()) {
+				return 1;
+			} else if(o.getCurriculo().getArtigos().size() > this.getCurriculo().getArtigos().size()) {
+				return -1;
+			} 
+			//premiacoes
+			else if(o.getCurriculo().getPremios().size() < this.getCurriculo().getPremios().size()) {
+				return 1;
+			} else if(o.getCurriculo().getPremios().size() > this.getCurriculo().getPremios().size()) {
+				return -1;
+			} 
+			//participacao em eventos
+			else if (o.getCurriculo().getEventos().size() < this.getCurriculo().getEventos().size()) {
+				return 1;
+			} else if (o.getCurriculo().getEventos().size() > this.getCurriculo().getEventos().size()) {
+				return -1;
+			} else {
+				return 0;
+			}
 		}
 	}
 
