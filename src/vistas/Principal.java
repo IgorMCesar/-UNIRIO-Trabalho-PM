6242cef1ebc06ac95a08405be00fea1a3e7eab44
TomasMachado <tomas.machado@uniriotec.br>
2018-11-26 01:05:25 -0200

    Principios da classe de vista Principal

--- src/vistas/Principal.java
+++ src/vistas/Principal.java
@@ -1,7 +1,10 @@
 package vistas;
 
-import utilidades.XML;
+import java.util.Scanner;
+
 import org.w3c.dom.Document;
+
+import utilidades.XML;
 
 public class Principal {
 
@@ -13,6 +16,76 @@ public class Principal {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	
+	
+	
+	public Principal() {
+		
+		while(true) {
+			
+			String resposta = lerReposta();
+			
+			decifrarResposta(resposta);
+			
+			
+		}
+
+	}
+	
+	
+	
+	private void decifrarResposta(String resposta) {
+		
+		String[] valores = resposta.split(" ");
+		
+		String opcao = valores[0];
+		
+		String argumento = valores[1];
+		
+		switch (opcao) {
+		case "-o":
+			break;//TODO chamar controller
+	case "-l":    
+			break;//TODO chamar controller
+	case "-a":    
+			break;//TODO chamar controller
+	case "-c":    
+			break;//TODO chamar controller
+	case "-v":    
+			break;//TODO chamar controller
+	case "-pr":   
+			break;//TODO chamar controller
+	case "-ar":   
+			break;//TODO chamar controller
+	case "-anr":  
+			break;//TODO chamar controller
+	case "-pe":   
+			break;//TODO chamar controller
+	case "-vi":
+		break;
+
+		default:
+			System.out.println("Opção não encontrada.");
+		}
+		
+	}
+
+
+
+
+
+	private String lerReposta() {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String resposta = scanner.nextLine();
+		
+		scanner.close();
+		
+		return resposta;
 	}
 
 }
