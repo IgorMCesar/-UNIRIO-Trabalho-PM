83111acf0ff9f6a56a381da70921858f462e8f69
Lucas Pimenta <lucas.pimenta@uniriotec.br>
2018-11-27 13:46:12 -0200

    Método para instanciar o currículo

--- src/main/java/controladores/CurriculoControlador.java
+++ src/main/java/controladores/CurriculoControlador.java
@@ -1,5 +1,22 @@
 package controladores;
 
-public class CurriculoControlador {
+import org.w3c.dom.Document;
 
+import modelos.Curriculo;
+import utilidades.XML;
+
+public class CurriculoControlador {
+	XML xml = new XML();
+
+	public Curriculo instanciarCurriculo(Document documento){
+		Curriculo curriculo = new Curriculo();
+		curriculo.setEventos(xml.instanciarEventos(documento));
+		curriculo.setArtigos(xml.instanciarArtigos(documento));
+		curriculo.setPremios(xml.instanciarPremios(documento));
+		//TODO instanciar semestres cursados
+		
+		
+	return curriculo;
+		
+	}
 }
