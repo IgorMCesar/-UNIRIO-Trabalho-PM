3ace803f4b28b135859434cb2496b302492a626c
IgorMCesar <igormarracho01@gmail.com>
2018-11-28 19:32:56 -0200

    Documentacao da classe de comparacao

--- src/main/java/utilidades/ComparadorUtil.java
+++ src/main/java/utilidades/ComparadorUtil.java
@@ -5,7 +5,12 @@ import info.debatty.java.stringsimilarity.*;
 public class ComparadorUtil {
 	
 	private static double VALOR_DISTANCIA_MINIMA = 0.01;
-	
+	/**
+	 * Compara distancia entre duas strings, a menor distancia indica a maior similidade
+	 * @param periodico String vinda do curriculo
+	 * @param dado String vinda do csv fornecido
+	 * @return Boolean se é semelhante
+	 */
 	public boolean comparaSemelhante(String periodico, String dado) {
 		JaroWinkler jaroWinkler = new JaroWinkler();
 		double distanciaDeString = jaroWinkler.distance(periodico.toLowerCase(), dado.toLowerCase());
