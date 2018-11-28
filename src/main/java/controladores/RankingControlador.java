7948c965a718424f5be4e315ff35d7e22e088a97
LucasMoraisUNIRIO <lucas.lopes@uniriotec.br>
2018-11-28 02:26:45 -0200

    Trabalhando Controlador de Ranking

--- src/main/java/controladores/RankingControlador.java
+++ src/main/java/controladores/RankingControlador.java
@@ -1,5 +1,14 @@
 package controladores;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import modelos.Aluno;
+
 public class RankingControlador {
 	
+	public ArrayList<Aluno> gerarRanking(ArrayList<Aluno> alunos) {
+		Collections.sort(alunos);
+		return alunos;
+	}
 }
