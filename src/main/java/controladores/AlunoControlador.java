1cc1cee20dfafc5c7b0ca9047d2d0fbfe59252fc
Lucas L. Morais <lucas.lopes@uniriotec.br>
2018-12-02 19:22:50 -0200

    Update do controller

--- src/main/java/controladores/AlunoControlador.java
+++ src/main/java/controladores/AlunoControlador.java
@@ -1,7 +1,5 @@
 package controladores;
 
-import java.util.ArrayList;
-
 import org.w3c.dom.Document;
 
 import modelos.Aluno;
@@ -13,8 +11,6 @@ public class AlunoControlador {
 
 	public Aluno instanciarAluno(String caminhoXML, Integer numeroSemestresSemReprovacao) {
 
-		XML xml = new XML();
-
 		Document documento = null;
 		try {
 			documento = (Document) curriculoControlador.instanciarCurriculo(caminhoXML, numeroSemestresSemReprovacao);
@@ -23,47 +19,15 @@ public class AlunoControlador {
 			e1.printStackTrace();
 		}
 
-		Aluno aluno = null;
-		try {
-			documento = XML.lerXml(caminhoXML);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		aluno.getCurriculo().setEventos(xml.instanciarEventos(documento));
-		aluno.getCurriculo().setArtigos(xml.instanciarArtigos(documento));
-		aluno.getCurriculo().setPremios(xml.instanciarPremios(documento));
-		aluno.getCurriculo().setVinculoUnirio(xml.instanciarVinculosUNIRIO(documento));
-		// TODO instanciar semestres cursados
-
-		aluno.getCurriculo().setNumeroSemestresSemReprovacao(numeroSemestresSemReprovacao);
-
-		aluno.setCurriculo(aluno.getCurriculo());
-
+		Aluno aluno = new Aluno(caminhoXML, numeroSemestresSemReprovacao);
+		
 		aluno.setNome(XML.pegarElementoDoCurriculo(documento, "NOME-COMPLETO", "DADOS-GERAIS"));
+		
+		aluno.setPontuacao(aluno.getCurriculo().calcularNota());
 
 		return aluno;
 
 	}
 	
-	/**
-	 * Insere um novo aluno para que o mesmo possa ser pontuado
-	 * 
-	 * @param caminhoXML
-	 *            String com o caminho do xml fornecido
-	 * @param numeroSemestresSemReprovacao
-	 *            Integer com o numero de semestres sem reprovacao na pos graduacao
-	 * @throws Exception
-	 */
-	public void inserirAluno(String caminhoXML, Integer numeroSemestresSemReprovacao, ArrayList<Aluno> alunos)
-			throws Exception {
-
-		Aluno aluno = new Aluno(caminhoXML, numeroSemestresSemReprovacao);
-
-		aluno.setCurriculo(curriculoControlador.instanciarCurriculo(caminhoXML, numeroSemestresSemReprovacao));
-
-		alunos.add(aluno);
-
-	}
 	
 }
